ebb80da568abdda2bc3292bc3a959ddb084c3d07
tbrenenstuhl <tdb.datascience@gmail.com>
2016-02-23 17:05:40 -0700

    Added files via upload

---
+++ timeseries_new.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 15:38:23 2016
+
+@author: Tyler
+"""
+
+#import modularized ts functions
+import tsfunctions as tsf
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+#lamda function to define date format
+dateparse = lambda dates: pd.datetime.strptime(dates, '%m/%d/%Y')
+#requires input from stdin
+#fileLocation = input ("Please list file location: ")
+# tab delimited; specify the time column
+data = pd.read_csv('C:\Users\Tyler\Desktop\GoDaddy_Data\student_data_20160215\use_this_data.csv', sep="\t",parse_dates='orderdate', index_col='orderdate',date_parser=dateparse)
+
+#set parameters
+#%matplotlib inline
+plt.rcParams['figure.figsize'] = 15, 6
+
+#run the ts code
+tsf.ts_run()
+
+#run module functions
+tsf.ts_plot(tsf.ts_gcr)
+tsf.ts_rollingStats(tsf.ts_gcr)
+tsf.ts_df(tsf.ts_gcr)
---
+++ tsfunctions.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 14:25:56 2016
+
+@author: MSBA Team B-03
+"""
+# ACF & PACF Plots:
+# from statsmodels.tsa.stattools import acf, pacf
+
+print ()
+
+def ts_run():
+	import pandas as pd
+	import timeseries_new as tsn	
+	#metric variables from data file columns
+	ts_gcr = tsn.data['gcr'] 
+	ts_orders = tsn.data['orders'] 
+	ts_months = tsn.data['product_months']
+	ts_units = tsn.data['product_units']
+	ts_gcrmonth = tsn.data['gcr'] / data['product_months']
+	ts_gcrunits = tsn.data['gcr'] / data['product_units']
+	ts_monthsunit = tsn.data['product_months'] / data['product_units']
+	#subregion lists from respective regions
+	apac = ['India', 'China', 'Australia', 'APAC-Tier2', 'APAC-Tier3']
+	emea = ['Turkey', 'United Kingdom', 'EMEA-Tier2', 'EMEA-Tier3']
+	latam = ['Mexico', 'Brazil', 'LATAM-Tier2', 'LATAM-Tier3']
+	others = ['Others-Tier3']
+	usa = ['United States']
+	canada = ['Canada']	
+	#list of metric variables
+	metrics = [ts_gcr, ts_orders, ts_months, ts_units, ts_gcrmonth, ts_gcrunits, ts_monthsunit]
+	#list of regions
+	subregions = [apac, emea, latam, canada, usa, others]
+	#list of product id's
+	prod_id = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+	
+	for m, r in zip(metrics, subregions):
+		data_subset=tsn.data[(tsn.data.report_region_2==subregions) & (tsn.data.product_category_id==prod_id)]
+		data_subset.sortlevel(['orderdate'],inplace=True)
+		
+#Plot metric
+def ts_plot(metric):
+	import matplotlib.pylab as plt
+	plt.plot(metric)
+
+#Plot rolling statistics:
+def ts_rollingStats(metric, window=30):
+	import pandas as pd
+	import matplotlib.pylab as plt
+	rolmean = pd.rolling_mean(metric, window=30)
+	rolstd = pd.rolling_std(metric, window=30)
+	orig = plt.plot(metric, color='blue',label='Original')
+	mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+	std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+	plt.legend(loc='best')
+	plt.title('Rolling Mean & Standard Deviation')
+	plt.show(block=False)
+    
+#Perform Dickey-Fuller test:
+def ts_df(metric):
+	import pandas as pd
+	from statsmodels.tsa.stattools import adfuller
+	print ('Results of Dickey-Fuller Test:')
+	dftest = adfuller(metric, autolag='AIC')
+	dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+	for key,value in dftest[4].items():
+	    dfoutput['Critical Value (%s)'%key] = value
+	print (dfoutput)
+
+
+#def ts_acf(x):
+#	lag_acf = acf(ts_log_diff, nlags = 20)
+#	plt.sublpot 
